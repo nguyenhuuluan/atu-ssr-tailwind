89efb3f03d34ae5e3d6eee8964aa332d1c27aa3a
Luan Nguyen <huuluanpy@gmail.com>
2021-01-26 01:37:42 +0700

    [Page]: Contact page - 80% => UI mobile not completed

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 import withWebAppLayout from '../../components/Layout/webapp.layout';
+import { COLORS } from '../../utils/constants';
 
 const Section = styled.section`
+  --padding: 1rem;
+  --margin: 2rem;
   position: relative;
   min-height: 100vh;
   background-color: none;
-  /* background-color: #112d42; */
-  /* z-index: 1; */
-  padding: 2rem;
-  ::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 50%;
-    height: 100%;
-    background: #03a9f4;
-  }
 
   & .container {
     /* margin: 2rem; */
+    height: 80vh;
+    padding: var(--padding);
   }
 
   & h1 {
@@ -41,6 +34,7 @@ const Section = styled.section`
       cursor: pointer;
       align-items: stretch;
       opacity: 0.5;
+      transition: all 0.2s ease-in;
 
       & span {
         margin: auto 0;
@@ -51,66 +45,54 @@ const Section = styled.section`
       }
       &:hover {
         opacity: 1;
+        transform: translateX(1rem) scale(1.1);
       }
     }
   }
 
-  /* & .container {
-    min-height: 550px;
-  } */
-
   & .contact-info {
-    /* position: absolute; */
     top: 40px;
-    width: 350px;
-    /* height: calc(100% - 80px); */
-    background: #0f3959;
+    background: #e66813;
     padding: 1rem;
     display: flex;
     border-radius: 0.75rem;
     flex-direction: column;
     justify-content: space-between;
     box-shadow: 0 20px 20px rgba(0, 0, 0, 0.2);
-    /* z-index: 1; */
-    /* ::before {
-      --margin: 2.5rem;
-      content: '';
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: calc(50% - 2 * var(--margin));
-      height: calc(100% - 2 * var(--margin));
-      background: red;
-      margin: var(--margin);
-      z-index: -1;
-    } */
+    z-index: 2;
   }
   & .contact-form {
-    /* position: absolute; */
-    top: 40px;
+    position: absolute;
     width: 100%;
-    /* height: calc(100% - 80px); */
-    background: #0f3952;
-    padding: 1rem;
-    display: flex;
-    border-radius: 0.75rem;
-    flex-direction: column;
-    justify-content: space-between;
-    box-shadow: 0 20px 20px rgba(0, 0, 0, 0.2);
+    height: calc(100% - 80px);
+    right: 0;
+    flex: 1;
     z-index: 1;
 
-    /* ::before {
-      --margin: 2.5rem;
-      content: '';
+    & .formBox {
+      /* background-color: #e5e7eb; */
+      /* margin-left: 5rem; */
       position: absolute;
       top: 0;
       right: 0;
-      width: calc(50% - 2 * var(--margin));
-      height: calc(100% - 2 * var(--margin));
-      background: blue;
-      margin: var(--margin);
-      z-index: -1;
-    } */
+      width: calc(100% - 2 * var(--margin) - 2 * var(--padding));
+      height: 100%;
+      padding: var(--padding);
+
+      text-align: center;
+      ::before {
+        content: '';
+        position: absolute;
+        top: 0;
+        right: 0;
+        width: 100%;
+        height: 100%;
+        background: #d2d2c2;
+        box-shadow: 0 20px 20px rgba(0, 0, 0, 0.2);
+        border-radius: 0.75rem;
+        z-index: -1;
+      }
+    }
   }
 
   & .sci {
@@ -118,8 +100,7 @@ const Section = styled.section`
     color: #fff;
     flex-direction: row;
     --tw-space-x-reverse: 0;
-    margin-top: 2rem;
-    margin-right: 0.5rem;
+    margin: 0 auto 2rem auto;
     /* margin-left: calc(1rem * calc(1 - var(--tw-space-x-reverse))); */
     font-size: 2.5rem;
     line-height: 2rem;
@@ -133,13 +114,52 @@ const Section = styled.section`
       }
     }
   }
+  & .inputBox {
+    position: relative;
+    margin-bottom: 1.5rem;
+
+    & input,
+    textarea {
+      width: 100%;
+      padding: 5px 0;
+      font-size: 1.2rem;
+      font-weight: 300;
+      color: #333;
+      border-bottom: 1px solid #777;
+      outline: none;
+
+      &:focus,
+      &:valid {
+        & ~ span {
+          transform: translateY(-1.5rem);
+          font-size: 12px;
+          font-weight: 400;
+          letter-spacing: 1px;
+          color: #ff568c;
+        }
+      }
+    }
+    & textarea {
+      min-height: 120px;
+    }
+
+    & span {
+      position: absolute;
+      left: 0;
+      /* padding: 5px 0; */
+      font-weight: 300;
+      color: #333;
+      pointer-events: none;
+      transition: 0.5s;
+    }
+  }
 `;
 
 const ContactPage = () => (
   <Section>
-    <div className="container relative flex items-center md:items-stretch flex-col md:flex-row">
+    <div className="container relative flex items-center flex-col md:flex-row">
       <div className="contact-info">
-        <h1>Contact Info</h1>
+        <h1 className="text-center">Contact Info</h1>
         <ul className="info">
           <li>
             <span>
@@ -172,44 +192,46 @@ const ContactPage = () => (
             </span>
           </li>
         </ul>
-        <ul className="sci space-x-4 text-2x">
+        <ul className="sci space-x-4">
           <li>
             <a href="#a">
-              <i class="fab fa-facebook-square" />
+              <i className="fab fa-facebook-square" />
             </a>
           </li>
           <li>
             <a href="#a">
-              <i class="fab fa-instagram" />
+              <i className="fab fa-instagram" />
             </a>
           </li>
         </ul>
       </div>
-      <div className="contact-form flex-1">
-        <h1>Send a message</h1>
+      <div className="contact-form flex-1 ">
         <div className="formBox">
-          <div className="inputBox w50">
-            <input type="text" name="" required />
-            <span>First Name</span>
-          </div>
-          <div className="inputBox w50">
-            <input type="text" name="" required />
-            <span>Last Name</span>
-          </div>
-          <div className="inputBox w50">
-            <input type="text" name="" required />
-            <span>Email Address</span>
-          </div>
-          <div className="inputBox w50">
-            <input type="text" name="" required />
-            <span>Mobile Number</span>
-          </div>
-          <div className="inputBox w100">
-            <textarea type="text" name="" required />
-            <span>Write your message here...</span>
-          </div>
-          <div className="inputBox w100">
-            <input type="submit" value="Send" />
+          <h1>Send a message</h1>
+          <div className="w-3/4 p-4 float-right  flex relative justify-between flex-wrap">
+            <div className="inputBox w-6/12">
+              <input className="w-full" type="text" name="" required />
+              <span>First Name</span>
+            </div>
+            <div className="inputBox w-6/12">
+              <input type="text" name="" required />
+              <span>Last Name</span>
+            </div>
+            <div className="inputBox w-6/12">
+              <input type="text" name="" required />
+              <span>Email Address</span>
+            </div>
+            <div className="inputBox w-6/12">
+              <input type="text" name="" required />
+              <span>Mobile Number</span>
+            </div>
+            <div className="inputBox w-full">
+              <textarea type="text" name="" required />
+              <span>Write your message here...</span>
+            </div>
+            <div className="inputBox w-full">
+              <input type="submit" value="Send" />
+            </div>
           </div>
         </div>
       </div>
--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,10 @@
-export const COLORS = { PRIMARY: '#ffb606', YELLOW: '#ffb606', BROWN: '#7f390a' };
+export const COLORS = {
+  DEFAULT: '#e5e7eb',
+  PRIMARY: '#ffb606',
+  SECONDARY: '#7f390a',
+  YELLOW: '#ffb606',
+  BROWN: '#7f390a',
+};
 export const PAGES = [
   { href: '/', title: 'Trang chủ' },
   { href: '/contact', title: 'Liên hệ' },
